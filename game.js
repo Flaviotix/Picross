3d158e6f5b62aa33e0d64d2afa386b724a9df4b1
Flaviotix <[flavionannini@gmail.com]>
2022-10-16 13:12:13 +0200

    déplacer image

--- game.js
+++ game.js
@@ -1,16 +1,74 @@
 let img;
-let x = 0;
-let y = 0;
+let img2;
+let timer;
 
-function load() {
-  img = new Sprite("images/ship.png");
+let KeyRight = false;
+let KeyLeft = false;
+let KeyUp = false;
+let KeyDown = false;
+
+function toucheEnfoncee(t) {
+  t.preventDefault();
+  if (t.code == "ArrowRight") {
+    KeyRight = true;
+  }
+  if (t.code == "ArrowLeft") {
+    KeyLeft = true;
+  }
+  if (t.code == "ArrowUp") {
+    KeyUp = true;
+  }
+  if (t.code == "ArrowDown") {
+    KeyDown = true;
+  }
+}
+function toucheRelache(t) {
+  t.preventDefault();
+  if (t.code == "ArrowRight") {
+    KeyRight = false;
+  }
+  if (t.code == "ArrowLeft") {
+    KeyLeft = false;
+  }
+  if (t.code == "ArrowUp") {
+    KeyUp = false;
+  }
+  if (t.code == "ArrowDown") {
+    KeyDown = false;
+  }
 }
 
-function update() {
-  x++;
-  y++;
+function load() {
+  document.addEventListener("keydown", toucheEnfoncee, false);
+  document.addEventListener("keyup", toucheRelache, false);
+
+  img = new Sprite("images/ship.png");
+  img2 = new Sprite("images/ship2.png");
+  timer = 0;
+}
+
+function update(dt) {
+  timer += dt;
+  if (timer >= 1) {
+    img.x += 10;
+    img.y += 10;
+    timer = 0;
+  }
+  if (KeyRight) {
+    img2.x += 2;
+  }
+  if (KeyLeft) {
+    img2.x -= 2;
+  }
+  if (KeyUp) {
+    img2.y -= 2;
+  }
+  if (KeyDown) {
+    img2.y += 2;
+  }
 }
 
 function draw(pCtx) {
   img.draw(pCtx);
+  img2.draw(pCtx);
 }
--- main.js
+++ main.js
@@ -2,9 +2,14 @@ let interval;
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 
+let derniereUpdate = Date.now();
+
 function run() {
+  let maintenant = Date.now();
+  let dt = (maintenant - derniereUpdate) / 1000;
+  derniereUpdate = maintenant;
   console.log("run");
-  update();
+  update(dt);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   draw(ctx);
 }
